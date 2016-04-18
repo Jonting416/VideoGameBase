71e5fb7d21d52d89cf74ae91e69ea0db859b8e63
Jon Ting <jting416@gmail.com>
2016-04-18 16:35:50 -0400

    Set up file system and index.html
    Added the necessary information and made a preliminary navigation bar
    for the main page index.html

---
+++ header.php
@@ -0,0 +1,28 @@
+<!--DEPRECATED DON"T USE THIS-->
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Video Gamebase - For all your new gaming needs</title>
+		<!--The following part of head is all bootstrap initialization-->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<!--This line is adding in the custom css sheet-->
+		<link rel="stylesheet" href="main.css">
+	</head>
+	<body>
+		<div class="nav">
+			<div class="container">
+				<ul class="pull-left">
+					<li><a href="index.html">Home</a></li>
+					<li><a href="#">Browse</a></li>
+				</ul>
+				<ul class="pull-right">
+					<li><a href="#">Sign Up</a></li>
+					<li><a href="#">Log In</a></li>
+					<li><a href="#">Help</a></li>
+				</ul>
+			</div>
+		</div>
