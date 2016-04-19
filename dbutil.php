47763ae471010be9d7bae9f3acff6147617882e3
Jon Ting <jting416@gmail.com>
2016-04-18 20:35:58 -0400

    Added opening head tag to login/signup, added dbutil.php
    Now can access database!

---
+++ dbutil.php
@@ -0,0 +1,22 @@
+<?php
+class DbUtil{
+	public static $loginUser = "cs4750jt4ue"; 
+	public static $loginPass = "H8KQ27YTB4hGQu4L";
+	public static $host = "stardock.cs.virginia.edu"; // DB Host
+	public static $schema = "cs4750jt4ue"; // DB Schema
+	
+	public static function loginConnection(){
+		$db = new mysqli(DbUtil::$host, DbUtil::$loginUser, DbUtil::$loginPass, DbUtil::$schema);
+	
+		if($db->connect_errno){
+			echo("Could not connect to db");
+			$db->close();
+			exit();
+		}
+		
+		return $db;
+	}
+	
+}
+?>
+
