b3ce3730988eedadc67a4d3fa804c0d915ffd8fa
Jing-Shuan <jlc6zj@virginia.edu>
2016-04-19 23:16:41 -0400

    Login and Signup works
    Login now functions correctly, Signup will now populate the Login table
    and the User table.

--- check_login.php
+++ check_login.php
@@ -1,11 +1,83 @@
 <!DOCTYPE html>
 <html lang="en">
+<!-- Script for popup alert if the username/password combination is wrong-->
+<script>
+function popupAlert(){
+	alert("This username/password combination does not exist!");
+}
+</script>
+Login PHP page<br>
+TODO<br>
+Make popup alert to let user know that a username/password is wrong. 
+Need to learn how to run the popupAlert() script.
+Create a cookie or session to keep a user logged in.
+
+<head>
+		<!--The following part of head is all bootstrap initialization-->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<!--This line is adding in the custom css sheet-->
+		<link rel="stylesheet" href="css/main.css">
+	</head>
+	<body>
+		<div class="nav">
+			<!--Container class is in bootstrap to make things look pretty-->
+			<div class="container">
+				<ul class="pull-left">
+					<li><a href="index.html">Home</a></li>
+					<li><a href="#">Browse</a></li>
+					<li>
+						<form id="searchbox" action="">
+							<input id="search" type="text" placeholder="Type in query here">
+							<input id="submit" type="submit" value="Search">
+						</form>
+					</li>
+				</ul>
+				<ul class="pull-right">
+					<li><a href="signup.html">Sign Up</a></li>
+					<li><a href="login.html">Log In</a></li>
+					<li><a href="help.html">Help</a></li>
+				</ul>
+			</div>
+		</div>
 <body>
-<?php
+
+<?php 
+$flag = FALSE;
 require "dbutil.php";
 $db = DbUtil::loginConnection();
-$stmt = $db->stmt_init();
-echo "Hello World";
+
+/* Check to make sure that the Username requested is not already in use
+If already in use: Return alert message saying to pick a different username
+If not already in use: Add the Username and Password to the DB 
+*/
+$sql = ("SELECT username, password FROM Login");
+$result = $db->query($sql);
+if ($result->num_rows > 0){
+	while ($row = $result->fetch_assoc()){ //Search through every row
+		if($row["username"] == $_POST["Username"] && $row["password"] == $_POST["Password"]){
+			$flag = TRUE; 
+		}
+	}
+}
+	/*If the username and password combination exist then flag will be TRUE and we want to 
+	login the user. We will want to use a session or cookie to keep that user state until 
+	they close the session. Otherwise when flag is FALSE the username/password combination
+	does not exist and we want to display a popup alert saying so.
+*/
+
+	if ($flag == TRUE){
+		 echo "You are now logged in!";
+		 //SET USER STATE TO LOGGED IN USING SESSION OR COOKIE
+	}
+	else if ($flag == FALSE){
+		echo "Incorrect login!";
+		// popupAlert(); //DOES NOT WORK
+	}
+
 $db->close;
 ?>
 </body>
--- check_signup.php
+++ check_signup.php
@@ -6,30 +6,84 @@ function popupAlert(){
 	alert("This username/password combination is already being used!");
 }
 </script>
+Sign-up PHP page<br>
+TODO<br>
+Make popup alert to let user know that a username is already being used. 
+Need to learn how to run the popupAlert() script.
 
+<head>
+		<!--The following part of head is all bootstrap initialization-->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<!--This line is adding in the custom css sheet-->
+		<link rel="stylesheet" href="css/main.css">
+	</head>
+	<body>
+		<div class="nav">
+			<!--Container class is in bootstrap to make things look pretty-->
+			<div class="container">
+				<ul class="pull-left">
+					<li><a href="index.html">Home</a></li>
+					<li><a href="#">Browse</a></li>
+					<li>
+						<form id="searchbox" action="">
+							<input id="search" type="text" placeholder="Type in query here">
+							<input id="submit" type="submit" value="Search">
+						</form>
+					</li>
+				</ul>
+				<ul class="pull-right">
+					<li><a href="signup.html">Sign Up</a></li>
+					<li><a href="login.html">Log In</a></li>
+					<li><a href="help.html">Help</a></li>
+				</ul>
+			</div>
+		</div>
 <body>
+
 <?php 
 $flag = FALSE;
 require "dbutil.php";
 $db = DbUtil::loginConnection();
+
+/* Check to make sure that the Username requested is not already in use
+If already in use: Return alert message saying to pick a different username
+If not already in use: Add the Username and Password to the DB 
+*/
 $sql = ("SELECT username FROM Login");
 $result = $db->query($sql);
 if ($result->num_rows > 0){
-	while ($row = $result->fetch_assoc()){
-		if($row["c_name"] == ){
-			$flag = TRUE;
+	while ($row = $result->fetch_assoc()){ //Search through every row
+		if($row["username"] == $_POST["Username"]){ //If name in given row == Username
+			$flag = TRUE; 
 		}
 	}
 }
-else {
-	echo "There are no entries";
-}
+	/*If the Username requested was found in the table Login, then flag will be true 
+and we'll want to send an alert. Otherwise we want to add the Username and Password
+into our database
+*/
 
-if ($flag == TRUE){
-	popupAlert()
-}
+	if ($flag == TRUE){
+		//popupAlert(); Right way to run the script?
+		 echo "username already taken test";
+	}
+	else if ($flag == FALSE){
+		echo "new account created";
+		$var1 = $_POST["Username"];
+		$var2 = $_POST["Password"];
+		$fName = $_POST["FirstName"];
+		$lName = $_POST["LastName"];
+		$email = $_POST["Email"];
+		$sql1 = "INSERT INTO Login (username, password) VALUES ('$var1','$var2')"; //apparently passing variables is a security flaw, idk why
+		$sql2 = "INSERT INTO User (username, first_name, last_name, email) VALUES ('$var1','$fName','$lName','$email')"; //No errors, but doesn't add anything into the table
+		$db->query($sql1);
+		$db->query($sql2);
+	}
 $db->close;
-
 ?>
 </body>
 </html>
