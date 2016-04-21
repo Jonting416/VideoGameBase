4300ffc1c78fad34e77e7fec5d4a592511e0327d
Jon Ting <jting416@gmail.com>
2016-04-21 13:46:54 -0400

    Set-up Add Game Page
    Log-in required for addgame.php and aside from that it's ok, need to set
    up the query to add records.

--- addgame.php
+++ addgame.php
@@ -23,7 +23,7 @@
 				<ul class="pull-left">
 					<li><a href="index.php">Home</a></li>
 					<li><a href="#">Browse</a></li>
-					<li><a href="addgame.html">Add a Game!</a></li>
+					<li><a href="addgame.php">Add a Game!</a></li>
 					<li>
 						<form id="searchbox" action="search.php">
 							<input id="search" type="text" placeholder="Type in query here">
@@ -37,6 +37,10 @@
 							echo "<li>Hello " . $_SESSION['User'] . "!</li>\n";
 							echo "<li><a href=\"logout.php\">Log Out</a></li>";
 						} else {
+							echo "<script type='text/javascript'>alert('Please login first!');</script>";
+							$URL="./index.php";
+							echo '<META HTTP-EQUIV="refresh" content="0;URL='.$URL.'">';
+							echo "<script type ='text/javascript'>document.location.href='{$URL}';</script>";
 							echo '<li><a href="signup.php">Sign Up</a></li>
 							<li><a href="login.php">Log In</a></li>';
 						}
@@ -45,8 +49,45 @@
 				</ul>
 			</div>
 		</div>
-		<center>
-			<legend>Add Game</legend>
-		</center>
+		<div class="container">
+			<legend><h2>Add Game</h2></legend>
+			<?php
+				echo "<p> Testing </p>";
+				if(isset($_POST)) {
+					echo "<p> Game name is " . $_POST["gamename"] . ".\n Pic URL is " . $_POST["picURL"] . ".</p>";
+					echo "<p> Genres selected: " . $_POST["genreForm"][0] . $_POST["genreForm"][1] . $_POST["genreForm"][2] . $_POST["genreForm"][3] . ".</p>";
+					foreach ($_POST as $key => $value) {
+				        echo "<tr>";
+				        echo "<td>";
+				        echo $key;
+				        echo "</td>";
+				        echo "<td>";
+				        echo $value;
+				        echo "</td>";
+				        echo "</tr>";
+				    }
+				}
+			?>
+			<form enctype="multipart/form-data" role="form" method="POST" action="addgame.php">
+				<div class="form-group">
+					<label for="gamename">Game Name:</label>
+					<input type="text" class="form-control" name="gamename">
+				</div>
+				<div class="form-group">
+					<label for="picURL">Cover Image (URL):</label>
+					<input type="text" class="form-control" name="picURL">
+				</div>
+				<div class="form-group">
+					<label for="genreForm">Select Genre (ctrl+click to select multiple)</label>
+					<select multiple="multiple" class="form-control" name="genreForm[]">
+						<option value="1">RPG</option>
+						<option value="2">Action</option>
+						<option value="3">FPS</option>
+						<option value="4">Adventure</option>
+					</select>
+				</div>
+				<button type="submit" class="btn btn-primary" id="submit">Submit</button>
+			</form>
+		</div>
 	</body>
 </html>
--- index.php
+++ index.php
@@ -22,7 +22,7 @@ session_start();
 				<ul class="pull-left">
 					<li><a href="index.php">Home</a></li>
 					<li><a href="#">Browse</a></li>
-					<li><a href="addgame.html">Add a Game!</a></li>
+					<li><a href="addgame.php">Add a Game!</a></li>
 					<li>
 						<form id="searchbox" action="search.php">
 							<input id="search" type="text" placeholder="Type in query here">
