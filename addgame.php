8328d631a93c214278a1ef26366079230310cc63
Jon Ting <jting416@gmail.com>
2016-04-20 20:06:39 -0400

    updated addgame.php navbar
    Brought it to current log-in standard

--- addgame.php
+++ addgame.php
@@ -23,18 +23,25 @@
 				<ul class="pull-left">
 					<li><a href="index.php">Home</a></li>
 					<li><a href="#">Browse</a></li>
-					<li><a href="addgame.php">Add a Game!</a></li>
+					<li><a href="addgame.html">Add a Game!</a></li>
 					<li>
-						<form id="searchbox" action="">
+						<form id="searchbox" action="search.php">
 							<input id="search" type="text" placeholder="Type in query here">
 							<input id="submit" type="submit" value="Search">
 						</form>
 					</li>
 				</ul>
 				<ul class="pull-right">
-					<li><a href="signup.html">Sign Up</a></li>
-					<li><a href="login.html">Log In</a></li>
-					<li><a href="help.html">Help</a></li>
+					<?php 
+						if(isset($_SESSION["User"])) {
+							echo "<li>Hello " . $_SESSION['User'] . "!</li>\n";
+							echo "<li><a href=\"logout.php\">Log Out</a></li>";
+						} else {
+							echo '<li><a href="signup.php">Sign Up</a></li>
+							<li><a href="login.php">Log In</a></li>';
+						}
+					?>
+					<li><a href="help.php">Help</a></li>
 				</ul>
 			</div>
 		</div>
