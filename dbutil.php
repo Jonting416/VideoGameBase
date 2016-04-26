0cb36c73fd037e932822e6051d8c522492ba9d21
Jon Ting <jting416@gmail.com>
2016-04-26 13:32:29 -0400

    Restored original dbutil
    Didn't work...for some reason login doesn't work anymore

--- dbutil.php
+++ dbutil.php
@@ -1,7 +1,12 @@
 <?php
 class DbUtil{
-	require "library.php";
+	//require "library.php";
 	
+	$SERVER = 'stardock.cs.virginia.edu';
+	$DATABASE = 'cs4750jt4ue';
+	$USERNAME = 'cs4750jt4ue';
+	$PASSWORD = 'H8KQ27YTB4hGQu4L';
+
 	public static function loginConnection(){
 		$db = new mysqli(DbUtil::$host, DbUtil::$loginUser, DbUtil::$loginPass, DbUtil::$schema);
 	
