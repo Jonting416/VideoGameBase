ca5549279089aab3966f008bc717adf90a55cab8
Jing-Shuan <jlc6zj@virginia.edu>
2016-04-27 18:31:04 -0400

    Removed uneeded code

--- check_signup.php
+++ check_signup.php
@@ -6,21 +6,19 @@ session_start();
 <html lang="en">
 Sign-up PHP page<br>
 <head>
-		<!--The following part of head is all bootstrap initialization-->
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-		<!--This line is adding in the custom css sheet-->
-		<link rel="stylesheet" href="css/main.css">
-	</head>
-	<div class="container">
-	<body>
-		<?php include 'header.php'; ?>
-	
+	<!--The following part of head is all bootstrap initialization-->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	<!--This line is adding in the custom css sheet-->
+	<link rel="stylesheet" href="css/main.css">
+</head>
+<div class="container">
+<body>
+<?php include 'header.php'; 
 
-<?php 
 $flag = FALSE;
 require "dbutil.php";
 $db = DbUtil::loginConnection();
