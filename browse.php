6226142f71eb8fcbcd83daa34379201942c4b20e
JingShuan <jlc6zj@virginia.edu>
2016-04-27 10:05:00 -0400

    browse now includes avg score from the Reviewer table

--- browse.php
+++ browse.php
@@ -23,14 +23,16 @@ $db = DbUtil::loginConnection();
 	$searchWord = "";
 	$sql = ("SELECT * FROM Game WHERE g_name LIKE '%$searchWord%'"); //Query for finding games
 	$sql1 = ("SELECT * FROM Console WHERE c_name LIKE '%$searchWord%'"); //Query for finding consoles
+	$sqlJoin = ("SELECT g_name, genre, msrp, p_name, g_id, AVG(score) AS Average FROM Game NATURAL JOIN Reviewer GROUP BY g_id"); 
 	$result = $db->query($sql);
 	$result1 = $db->query($sql1);
+	$resultJoin = $db->query($sqlJoin);
 	if ($result->num_rows>0 || $result1->num_rows>0){ //Looking in both game and console table for query
 		echo "<br>Results:";
 		echo '<table class="table table-striped">'; //putting results into a table
-		while($row = $result->fetch_assoc()){ //Checking game table
+		while($row = $resultJoin->fetch_assoc()){ //Checking game table
 			echo "<tr>" ;
-			echo '<td>'.$row["g_name"].'</td>'. "<td> Genre: ".$row["genre"]."</td><td> MSRP: $".$row["msrp"]."</td><td> Publisher: ".$row["p_name"]."</td>".
+			echo '<td>'.$row["g_name"].'</td>'. "<td> Genre: ".$row["genre"]."</td><td> MSRP: $".$row["msrp"]."</td><td> Publisher: ".$row["p_name"]."</td>"."<td>Average Review: ".$row["Average"]."</td>".
 				'<td><button type="button" onclick="loadInDepth("'.$row["g_name"].'")">In-Depth Information</button></td>';
 		}
 		while($row1 = $result1->fetch_assoc()){ //Checking console table
